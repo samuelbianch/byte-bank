ccae646065a84343e8b51327a79cdc7c35f1d6e2
samuelbianch <samuelbianch38@gmail.com>
2022-12-19 22:38:03 -0300

    Add: testando array

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,2 +1,14 @@
 ﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
+void TestaArray(){
+    int[] vetor = new int[5];
+
+    for(int i=0; i<vetor.Length; i++){
+        Console.WriteLine($"Digite o {i}° numero: ");
+        vetor[i] = Console.ReadLine();
+    }
+
+    for(int i=0; i<vetor.Length; i++){
+        Console.WriteLine(vetor[i]);
+    }
+}
